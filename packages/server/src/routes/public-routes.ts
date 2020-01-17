398ffdb95a6cdab85a9399f6bb1f5573c203cf21
Tim Feid <tim.feid@tellimer.com>
2020-01-17 13:23:32 -0500

    chore: add stream stuff

--- packages/server/src/routes/public-routes.ts
+++ packages/server/src/routes/public-routes.ts
@@ -1,11 +1,15 @@
 import Router from 'koa-router'
 import { AuthenticationController } from '../components/authentication/authentication.controller'
 import { CardController } from '../components/card/card.controller'
+import { NJDController } from '../components/njd/njd-controller'
 
 const publicRouter = new Router()
 
 publicRouter.post('/jwt', AuthenticationController.jwt)
 publicRouter.get('/cards', CardController.list)
+
+
+publicRouter.get('/ha.m3u8', NJDController.m3u8)
 
 export { publicRouter }
 
