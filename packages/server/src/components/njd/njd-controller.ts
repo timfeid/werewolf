7879f98dae5b4fce3d0de26858711c00846576a4
Tim Feid <tim.feid@tellimer.com>
2020-01-17 13:24:57 -0500

    chore: add stream stuff

---
+++ packages/server/src/components/njd/njd-controller.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { Context } from 'koa';
+
+export class NJDController {
+  public static async m3u8 (ctx: Context) {
+    const response = await axios.get("https://njd.zipstreams.net/ha.m3u8?url="+ctx.request.query.url);
+
+    ctx.body = response.data
+      .replace(/\/ha\.m3u8\?url=/g, 'http://wolf.timfeid.com/ha.m3u8?url=')
+      .replace(/#EXT-X-KEY:METHOD=AES-128,URI="/g, '#EXT-X-KEY:METHOD=AES-128,URI="https://njd.zipstreams.net')
+  }
+
+  public static async get (ctx: Context) {
+    const response = await axios.get("https://njd.zipstreams.net/ha.m3u8?url=" + ctx.request.query.url);
+
+    ctx.body = response.data
+      .replace(/\/ha\.m3u8\?url=/g, 'http://wolf.timfeid.com/ha.m3u8?url=')
+      .replace(/#EXT-X-KEY:METHOD=AES-128,URI="/g, '#EXT-X-KEY:METHOD=AES-128,URI="https://njd.zipstreams.net')
+  }
+}
