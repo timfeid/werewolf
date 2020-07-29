1f0f5497f5fd33ddfbe6f8322e7eab02fab0ac6b
Tim Feid <admin@example.com>
2020-07-29 01:57:20 +0000

    Update njd-controller.ts

--- packages/server/src/components/njd/njd-controller.ts
+++ packages/server/src/components/njd/njd-controller.ts
@@ -17,4 +17,10 @@ export class NJDController {
       .replace(/\/ha\.m3u8\?url=/g, 'https://wolf.timfeid.com/ha.m3u8?url=')
       .replace(/#EXT-X-KEY:METHOD=AES-128,URI="/g, '#EXT-X-KEY:METHOD=AES-128,URI="https://njd.zipstreams.net')
   }
+
+  public static async getUrl (ctx: Context) {
+    const response = await axios.get(`https://njd.zipstreams.net/getM3U8.php?id=${ctx.params.id}&league=nhl&date=${ctx.params.date}&cdn=akc`);
+
+    ctx.body = response.data
+  }
 }
