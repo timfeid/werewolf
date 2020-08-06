1d58e649750f30e9d01c9f49a5772f369e86c51b
Tim Feid <admin@example.com>
2020-08-06 16:44:34 +0000

    Update njd-controller.ts

--- packages/server/src/components/njd/njd-controller.ts
+++ packages/server/src/components/njd/njd-controller.ts
@@ -3,29 +3,29 @@ import { Context } from 'koa';
 
 export class NJDController {
   public static async m3u8 (ctx: Context) {
-    const response = await axios.get("https://njd.zipstreams.net/ha.m3u8?url="+ctx.request.query.url);
+    const response = await axios.get("https://puddy.zipstreams.net/ha.m3u8?url="+ctx.request.query.url);
 
     ctx.body = response.data
       .replace(/\/ha\.m3u8\?url=/g, 'https://wolf.timfeid.com/ha.m3u8?url=')
-      .replace(/#EXT-X-KEY:METHOD=AES-128,URI="/g, '#EXT-X-KEY:METHOD=AES-128,URI="https://njd.zipstreams.net')
+      .replace(/#EXT-X-KEY:METHOD=AES-128,URI="/g, '#EXT-X-KEY:METHOD=AES-128,URI="https://puddy.zipstreams.net')
   }
 
   public static async get (ctx: Context) {
-    const response = await axios.get("https://njd.zipstreams.net/ha.m3u8?url=" + ctx.request.query.url);
+    const response = await axios.get("https://puddy.zipstreams.net/ha.m3u8?url=" + ctx.request.query.url);
 
     ctx.body = response.data
       .replace(/\/ha\.m3u8\?url=/g, 'https://wolf.timfeid.com/ha.m3u8?url=')
-      .replace(/#EXT-X-KEY:METHOD=AES-128,URI="/g, '#EXT-X-KEY:METHOD=AES-128,URI="https://njd.zipstreams.net')
+      .replace(/#EXT-X-KEY:METHOD=AES-128,URI="/g, '#EXT-X-KEY:METHOD=AES-128,URI="https://puddy.zipstreams.net')
   }
   
   public static async proxy (ctx: Context) {
-    const response = await axios.get("https://njd.zipstreams.net/" + ctx.request.query.url);
+    const response = await axios.get("https://puddy.zipstreams.net/" + ctx.request.query.url);
 
     ctx.body = response.data
   }
 
   public static async getUrl (ctx: Context) {
-    const response = await axios.get(`https://njd.zipstreams.net/getM3U8.php?id=${ctx.params.id}&league=nhl&date=${ctx.params.date}&cdn=akc`);
+    const response = await axios.get(`https://puddy.zipstreams.net/getM3U8.php?id=${ctx.params.id}&league=nhl&date=${ctx.params.date}&cdn=akc`);
 
     ctx.body = response.data
   }
